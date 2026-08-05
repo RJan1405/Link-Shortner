6367b1aa29e4beedc89388e60a9a0e497af33acd
RJan1405 <bhusarahardik@gmail.com>
2026-08-05 22:20:47 +0530

    working?

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/', (req, res) => {
+    res.send('URL Shortener API is running')
+})
+
 app.get('/:shortId', handelClick)
 
 app.use('/api', routes)
